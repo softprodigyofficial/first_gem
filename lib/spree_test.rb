d9f9f84f6566902a95e39472f3ff7bb14d4ae1ee
rahulsps <rahul_bhola@softprodigy.com>
2018-06-08 17:34:38 +0530

    do http

--- lib/spree_test.rb
+++ lib/spree_test.rb
@@ -3,11 +3,16 @@ require "spree_test/version"
 module SpreeTest
   class Food
     def self.portray(food)
+      
+      response = HTTParty.get('http://api.stackexchange.com/2.2/questions?site=stackoverflow')
+	  puts response.body, response.code, response.message, response.headers.inspect
+	  
       if food.downcase == "broccoli"
         "Gross!"
       else
         "Delicious!"
       end
+      
     end
   end
 end

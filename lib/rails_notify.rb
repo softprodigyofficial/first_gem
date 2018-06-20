4fb93a59a8089ca3cfd1bcd31c29aa9bf47f686b
rahulsps <rahul_bhola@softprodigy.com>
2018-06-20 11:15:17 +0530

    make place for additional platforms

--- lib/rails_notify.rb
+++ lib/rails_notify.rb
@@ -1,12 +1,16 @@
 require "rails_notify/version"
 
 module RailsNotify
-	class Notification
-		
+    class Notification
+        
         def self.send(notifications)
             for notification in notifications
                 if(notification['platform'] == 'slack')
                     HTTParty.post(notification['url'], :body => notification['payload'].to_json, :headers => { 'Content-Type' => 'application/json' })
+                elsif(notification['platform'] == 'email')
+                    #send email here
+                else
+					#do nothing
                 end
             end
         end
